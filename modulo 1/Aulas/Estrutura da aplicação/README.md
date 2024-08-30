1e84091dcd2df3bff3b471e620747945acf78a92
shellookk <isauqejk123@gmail.com>
2024-08-30 13:54:45 -0300

    subindo repositório e aulas assistidas

---
+++ modulo 1/Aulas/Estrutura da aplicação/README.md
@@ -0,0 +1,5 @@
+
+Stateful -> salva os arquivos em uma memoria temporaria
+Stateless -> salva os arquivos no banco
+
+Cabeçalhos (Requisição/resposta) => Metadados
---
+++ modulo 1/Aulas/Streams no Node.js/fundamentals.js
@@ -0,0 +1,26 @@
+//stdin - stream de leitura
+//stdout - stream de saida
+import { Readable, Writable, Transform } from 'node:stream'
+
+class OneToHundredStream extends Readable {
+  index = 1
+}
+
+class InverseNumberStream extends Transform {
+  _transform(chunk, encoding, callback) {
+    const transformed = Number(chunk.toString()) * -1
+
+    callback(null, Buffer.from(String(transformed)))
+  }
+}
+
+class MultiplyByTenStream extends Writable {
+  _write(chunk, encoding, callback) {
+    console.log(Number(chunk.toString()) * 10)
+    callback()
+  }
+}
+
+new OneToHundredStream()
+  .pipe(new InverseNumberStream())
+  .pipe(new MultiplyByTenStream())
